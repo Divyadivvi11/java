d571ce42943d776cc3f179cfa88ffc6dec235687
Divyadivvi11 <43901625+Divyadivvi11@users.noreply.github.com>
2018-10-06 14:13:15 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main
         int b;
         Scanner s = new Scanner(System.in);
         System.out.println("");
-        n = s.nextInt();
+        b= s.nextInt();
         if(b > 0)
         {
             System.out.println(" Positive");
